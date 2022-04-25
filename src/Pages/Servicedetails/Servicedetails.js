d9da8e5f43b8dc920b8805d4bef385890a8cf389
solaiman15-11026 <samimunna49@gmail.com>
2022-04-25 17:29:11 +0600

    new

---
+++ src/Pages/Servicedetails/Servicedetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Servicedetails = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Servicedetails;
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import logo from '../../../images/logo.png'
 
 const Header = () => {
     return (
 
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar collapseOnSelect expand="lg" bg="primary" sticky='top' variant="dark">
             <Container>
-                <Navbar.Brand href="#home"><img width="150"
+                <Navbar.Brand as={Link} to="/"><img width="150"
                     height="40" src={logo} alt="" /></Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
@@ -23,9 +24,9 @@ const Header = () => {
                         </NavDropdown>
                     </Nav>
                     <Nav>
-                        <Nav.Link href="#deets">More deets</Nav.Link>
-                        <Nav.Link eventKey={2} href="#memes">
-                            Dank memes
+                        <Nav.Link as={Link} to="/about">About</Nav.Link>
+                        <Nav.Link as={Link} to="/login">
+                            Login
                         </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
